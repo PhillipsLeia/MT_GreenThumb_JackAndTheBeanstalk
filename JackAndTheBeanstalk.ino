229e0e6d4702ed3345b89c57ab5baf224e7d9b25
emilymiraecho <129880506+emilymiraecho@users.noreply.github.com>
2023-04-04 13:37:40 -0500

    Update JackAndTheBeanstalk.ino

--- JackAndTheBeanstalk.ino
+++ JackAndTheBeanstalk.ino
@@ -1,14 +1,14 @@
 #include <Servo.h>
 
-const int jackDownPin = 2;
-const int jackUpPin = 3;
-const int henDownPin = 4;
-const int allUpPin = 5;
-const int flapPin = 9;
-const int giantPin = 10;
+const int jackDownPin = 2;  //this is the pin for Jack being on the low level
+const int jackUpPin = 3;  //this is the pin for Jack being on the high level
+const int henDownPin = 4; //this is the pin for Hen being on the low level
+const int allUpPin = 5; //this is the pin for everything being on the high level
+const int flapPin = 9;  //this is the pin for the flap
+const int giantPin = 10;  //this is the pin for the giant 
 
-Servo flap;
-Servo giant;
+Servo flap; //servo for flap to be moved 
+Servo giant;  //servo for giant to be popped up
 
 void setup() {
   // put your setup code here, to run once:
