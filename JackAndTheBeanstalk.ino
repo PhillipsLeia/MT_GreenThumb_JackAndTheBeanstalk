9c3624027f46a4155af98877ba3d279e2d338ce0
PhillipsLeia <116131337+PhillipsLeia@users.noreply.github.com>
2023-04-08 13:38:16 -0500

    giant variables
    Added the variables needed for the giant to appear. Also commented them.

--- JackAndTheBeanstalk.ino
+++ JackAndTheBeanstalk.ino
@@ -10,10 +10,13 @@ const int giantPin = 10;  //this is the pin for the giant
 Servo flap; //servo for flap to be moved 
 Servo giant;  //servo for giant to be popped up
 
-int jackDownStatus = 0;
-int jackUpStatus = 0;
+int jackDownStatus = 0; //if jack is detected at the bottom of the beanstalk
+int jackUpStatus = 0; //if jack is detected at the top of the beanstalk
+int henDownStatus = 0; //if the hen is detected on the bottom floor
+int allUpStatus = 0; //if all of the giants stuff is detected on the top floor
 
-bool flapUp = true;
+bool flapUp = true; //if the flap is currently up
+bool giantOut = false; //if the giant is out
 
 void setup() {
   pinMode(jackDownPin, INPUT);
